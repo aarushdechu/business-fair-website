c88c165be78218826f22b34894fa8d168bc3688d
Aarush Dechu <aarushdechu@users.noreply.github.com>
2026-08-05 09:28:12 -0400

    Blend product cutouts into story background

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='sketchy-shell-v4';
+const CACHE='sketchy-shell-v5';
 const SHELL=['/','/styles.css','/app.js','/manifest.webmanifest','/assets/logo.png','/assets/products/origami-eagle-display.png','/assets/products/origami-dragon-v3-display.png','/assets/products/origami-mouse-display.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
